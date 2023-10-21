b04551199e51d5b3a94dda49723e74106ed84dde
Elmira PETIT <elledevelope@gmail.com>
2023-10-21 18:08:37 +0200

    First commit

---
+++ assets/constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        id: "el1",
+        name: "Bouquet Primavera",
+        img: "https://i.ibb.co/0fqR5qt/bouquet-1.jpg",
+        price: 65,
+    },
+    {
+        id: "el2",
+        name: "Bouquet Calís",
+        img: "https://i.ibb.co/52tYJJq/bouquet-3.jpg",
+        price: 45,
+    },
+    {
+        id: "el3",
+        name: "Bouquet Besame Mucho",
+        img: "https://i.ibb.co/bmh1zqw/bouquet-2.jpg",
+        price: 55,
+    },
+    {
+        id: "el4",
+        name: "Bouquet Cala Ferriol",
+        img: "https://i.ibb.co/N2kbnnQ/bouquet-4.jpg",
+        price: 65,
+    },
+    {
+        id: "el5",
+        name: "Bouquet Calella",
+        img: "https://i.ibb.co/g4ZJskn/bouquet-5.jpg",
+        price: 45,
+    },
+    {
+        id: "el6",
+        name: "Bouquet Besame Mucho",
+        img: "https://i.ibb.co/31vr6kG/bouquet-6.jpg",
+        price: 55,
+    },
+    {
+        id: "el7",
+        name: "Bouquet Primavera",
+        img: "https://i.ibb.co/6gxTrg9/bouquet-7.jpg",
+        price: 65,
+    },
+    {
+        id: "el8",
+        name: "Bouquet Calís",
+        img: "https://i.ibb.co/MsFHknK/bouquet-8.jpg",
+        price: 45,
+    },
+    {
+        id: "el9",
+        name: "Bouquet Besame Mucho",
+        img: "https://i.ibb.co/31vr6kG/bouquet-6.jpg",
+        price: 55,
+    },
+    {
+        id: "el10",
+        name: "Bouquet Besame Mucho",
+        img: "https://i.ibb.co/bmh1zqw/bouquet-2.jpg",
+        price: 55,
+    },
+];
---
+++ readme.md
@@ -0,0 +1,11 @@
+### resources:
+
+[Flaticon](https://www.flaticon.com/)
+
+[Upload and share your images](https://imgbb.com/)
+
+[Free Stock Photos & Images - Unsplash](https://unsplash.com/)
+
+[Icon library - Font Awesome](https://fontawesome.com/icons)
+
+[Get Emoji — All Emojis to ️ Copy and Paste](https://getemoji.com/)
